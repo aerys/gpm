1e0fb412f6f27fc2a4d779a973edcf0b876afc10
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2018-07-13 01:21:36 +0200

    Add missing -Force parameters in publish.ps1.

--- script/publish.ps1
+++ script/publish.ps1
@@ -1,20 +1,21 @@
-if (-Not $env:APPVEYOR_REPO_TAG) {
-    Write-Host "Not a tag: skip publishing."
-    exit 0    
-}
+# if (-Not $env:APPVEYOR_REPO_TAG) {
+#     Write-Host "Not a tag: skip publishing."
+#     exit 0    
+# }
 
 if ($env:target -ne "x86_64-pc-windows-msvc") {
     Write-Host "Target $env:target detected: skip publishing."
     exit 0
 }
 
-$version = $env:APPVEYOR_REPO_TAG_NAME
+# $version = $env:APPVEYOR_REPO_TAG_NAME
+$version = "0.4.3"
 $github_username = $env:GITHUB_USERNAME
 $github_token = $env:GITHUB_TOKEN
 
 git clone "https://${github_username}:${github_token}@github.com/aerys/gpm-packages.git" 2>&1 | Write-Host
-mkdir -p gpm-packages/gpm-windows64
-Compress-Archive -Path .\target\release\gpm.exe -DestinationPath .\gpm-packages\gpm-windows64\gpm-windows64.zip
+mkdir -Force -p gpm-packages/gpm-windows64
+Compress-Archive -Force -Path .\target\release\gpm.exe -DestinationPath .\gpm-packages\gpm-windows64\gpm-windows64.zip
 cd gpm-packages/gpm-windows64
 git config --global user.email "noreply@ci.appveyor.com" 2>&1 | Write-Host
 git config --global user.name "AppVeyor" 2>&1 | Write-Host
