c8d27e085938b11bed6655576899cc6e900eab5c
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2023-01-13 10:31:14 +0000

    Upgrade Rust to 1.66.0

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM ekidd/rust-musl-builder:1.51.0
+FROM registry.aerys.in/aerys/infrastructure/vendor/rust-docker/x86_64-unknown-linux-musl:1.66.0-1
 
-# We need to add the source code to the image because `rust-musl-builder`
-# assumes a UID of 1000, but TravisCI has switched to 2000.
-ADD . ./
-RUN sudo chown -R rust:rust .
+ENV UPX_VERSION=4.0.0
 
-CMD cargo build --release
+# Install UPX
+RUN cd /tmp && \
+    curl -fLO https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz && \
+    tar -xf upx-${UPX_VERSION}-amd64_linux.tar.xz && \
+    rm -rf upx-${UPX_VERSION}-amd64_linux.tar.xz && \
+    cd upx-${UPX_VERSION}-amd64_linux && \
+    chmod +x upx && \
+    cp ./upx /usr/bin/upx
--- script/build-release.sh
+++ script/build-release.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 #
-# Usage: ./build-release
+# Usage: ./build-release.sh
 #
-# The latest version of this script is available at
-# https://github.com/emk/rust-musl-builder/blob/master/examples/build-release
-#
-# Called by `.travis.yml` to build release binaries.  We use
-# ekidd/rust-musl-builder to make the Linux binaries so that we can run
-# them unchanged on any distro, including tiny distros like Alpine (which
-# is heavily used for Docker containers).  Other platforms get regular
-# binaries, which will generally be dynamically linked against libc.
-#
-# If you have a platform which supports static linking of libc, and this
-# would be generally useful, please feel free to submit patches.
 
 set -euo pipefail
 
+PROJECT_NAME="gpm"
+
 case `uname -s` in
     Linux)
-        echo "Building static binaries using ekidd/rust-musl-builder"
-        docker build -t build-gpm-image .
-        docker run --name build-gpm build-gpm-image
-        docker cp build-gpm:/home/rust/src/target/x86_64-unknown-linux-musl/release/gpm gpm
-        docker rm build-gpm
-        docker rmi build-gpm-image
-        strip gpm
-        docker run --rm -w $PWD -v $PWD:$PWD aerysinnovation/upx:latest --best --lzma gpm
+        echo "Building a static binary"
+        docker build -t build-${PROJECT_NAME}-image .
+        docker run --rm \
+            -v ${PWD}:${PWD} -w ${PWD} \
+            build-${PROJECT_NAME}-image \
+            bash -c "
+                cargo build --release --target=x86_64-unknown-linux-musl
+                strip target/x86_64-unknown-linux-musl/release/${PROJECT_NAME}
+                upx --best --lzma target/x86_64-unknown-linux-musl/release/${PROJECT_NAME}
+            "
+        docker rmi build-${PROJECT_NAME}-image
         ;;
     *)
         echo "Building standard release binaries"
